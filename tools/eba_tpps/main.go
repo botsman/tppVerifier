4f6c2e27b93f6a2135ebb80bd37a7f90da13f19c
Pavel Botsman <11509664+botsman@users.noreply.github.com>
2025-08-02 22:02:04 +0300

    improve tpp parsing

--- tools/eba_tpps/main.go
+++ tools/eba_tpps/main.go
@@ -11,6 +11,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"slices"
 	"strings"
 	"time"
 
@@ -20,27 +21,230 @@ import (
 	"github.com/botsman/tppVerifier/app/models"
 )
 
-type Service string
+type RawTPP struct {
+	CA_OwnerID            string
+	Code                  string
+	Type                  string
+	AuthorizedAt          time.Time
+	NationalReferenceCode string
+	Names                 []string
+	Country               string
+	Services              map[string][]models.Service
+}
 
-const (
-	AIS Service = "AISP"
-	PIS Service = "PISP"
-	// CBPII Service = "CBPII"
-)
+func (r *RawTPP) GetLatinName() string {
+	if len(r.Names) == 1 {
+		return r.Names[0]
+	}
+	if len(r.Names) > 1 {
+		// We should check that it contains only Latin characters
+		// But keep it simple for now
+		return r.Names[1]
+	}
+	return ""
+}
+
+func (r *RawTPP) GetNativeName() string {
+	if len(r.Names) == 1 {
+		return r.Names[0]
+	}
+	if len(r.Names) > 1 {
+		return r.Names[0]
+	}
+	return ""
+}
+
+func (r *RawTPP) toTPP() models.TPP {
+	authority := parseAuthority(r.CA_OwnerID, r.Country)
+	if authority == "" {
+		// log.Printf("Invalid authority for CA_OwnerID %s and Country %s, skipping TPP\n", r.CA_OwnerID, r.Country)
+		return models.TPP{}
+	}
+	obID, err := parseOBID(r.NationalReferenceCode, r.Country, authority)
+	if err != nil {
+		// log.Printf("Error parsing OBID for NationalReferenceCode %s, Country %s, Authority %s: %v, skipping TPP\n", r.NationalReferenceCode, r.Country, authority, err)
+		return models.TPP{}
+	}
+	tpp := models.TPP{
+		NameLatin:    r.GetLatinName(),
+		NameNative:   r.GetNativeName(),
+		Id:           r.Code,
+		OBID:         obID,
+		Authority:    authority,
+		Country:      r.Country,
+		Services:     r.Services,
+		AuthorizedAt: r.AuthorizedAt,
+		Type:         r.Type,
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+		Registry:     "EBA",
+	}
+
+	if len(r.Names) > 1 {
+		tpp.NameNative = r.Names[1]
+	}
+
+	return tpp
+}
+
+func (r *RawTPP) UnmarshalJSON(data []byte) error {
+	var raw map[string]any
+	if err := json.Unmarshal(data, &raw); err != nil {
+		return err
+	}
+	CA_OwnerID, ok := raw["CA_OwnerID"].(string)
+	if !ok {
+		// log.Println("CA_OwnerID is required, skipping this TPP")
+		return nil
+	}
+	r.CA_OwnerID = CA_OwnerID
+	code, ok := raw["EntityCode"].(string)
+	if !ok {
+		return errors.New("EntityCode is required")
+	}
+	r.Code = code
+
+	entType, ok := raw["EntityType"].(string)
+	if !ok {
+		// log.Println("Type is required, skipping this TPP")
+		return nil
+	}
+	r.Type = entType
+	authorizedAt, err := r.findProperty(raw["Properties"], "ENT_AUT")
+	if err != nil {
+		// log.Println("AuthorizedAt is required, skipping this TPP")
+		return nil
+	}
+	if len(authorizedAt) > 0 {
+		// pick the last one as it is the latest date
+		r.AuthorizedAt, _ = time.Parse(time.DateOnly, authorizedAt[len(authorizedAt)-1])
+	}
+	nationalReferenceCode, err := r.findProperty(raw["Properties"], "ENT_NAT_REF_COD")
+	if err != nil {
+		return err
+	}
+	r.NationalReferenceCode = nationalReferenceCode[0]
+	names, err := r.findProperty(raw["Properties"], "ENT_NAM")
+	if err != nil {
+		return err
+	}
+	r.Names = names
+	country, err := r.findProperty(raw["Properties"], "ENT_COU_RES")
+	if err != nil {
+		return err
+	}
+	r.Country = country[0]
+	services, err := r.parseServices(raw["Services"])
+	if err != nil {
+		return err
+	}
+	r.Services = services
+	return nil
+}
+
+func serviceFromString(str string) (models.Service, error) {
+	switch str {
+	case "PS_080":
+		return models.AISP, nil
+	case "PS_070":
+		return models.PISP, nil
+	}
+	return "", fmt.Errorf("unknown service: %s", str)
+}
+
+func parseOBID(entityNatRefCode string, country string, authority string) (string, error) {
+	// TPP Open banking ID is expected to be in the format: PSD{country}-{authority}-{id}
+	// Eg. PSDFI-FINFSA-0111027-9
+	// Id may or may not contain a dash at the end. For simplicity it will be removed
+	natRefCode := strings.ReplaceAll(entityNatRefCode, "-", "")
+	return fmt.Sprintf("PSD%s-%s-%s", country, authority, natRefCode), nil
+}
+
+func parseAuthority(authority string, country string) string {
+	// authority is expected to be in the format: {country}-{authority}
+	// Eg. "FI_FIN-FSA"
+	// The dash is removed if it exists
+	parts := strings.Split(authority, "_")
+	if len(parts) != 2 {
+		return ""
+	}
+	if parts[0] != country {
+		return ""
+	}
+	res := parts[1]
+	res = strings.ReplaceAll(res, "-", "")
+	return res
+}
+
+func parseCountry(properties []interface{}) string {
+	for _, prop := range properties {
+		if propMap, ok := prop.(map[string]interface{}); ok {
+			if country, ok := propMap["ENT_COU_RES"].(string); ok {
+				return country
+			}
+		}
+	}
+	return ""
+}
+
+func (r *RawTPP) parseServices(servicesData any) (map[string][]models.Service, error) {
+	// Services are represented aither as []map[string]string or as []map[string][]string
+	// depending if the country has multiple services or not
+	res := make(map[string][]models.Service)
+	if servicesData == nil {
+		return res, nil
+	}
+	for _, countryServices := range servicesData.([]any) {
+		for country, services := range countryServices.(map[string]interface{}) {
+			// service may be either a string or an array of strings
+			switch service := services.(type) {
+			case string:
+				s, err := serviceFromString(service)
+				if err != nil {
+					continue
+				}
+				res[country] = append(res[country], s)
+			case []interface{}:
+				for _, service := range service {
+					s, err := serviceFromString(service.(string))
+					if err != nil {
+						continue
+					}
+					res[country] = append(res[country], s)
+				}
+			}
+		}
+	}
+	return res, nil
+}
+
+func (r *RawTPP) findProperty(properties any, key string) ([]string, error) {
+	if props, ok := properties.([]any); ok {
+		for _, prop := range props {
+			if propMap, ok := prop.(map[string]any); ok {
+				if value, ok := propMap[key].(string); ok {
+					return []string{value}, nil
+				} else if values, ok := propMap[key].([]any); ok {
+					strValues := make([]string, len(values))
+					for i, v := range values {
+						if str, ok := v.(string); ok {
+							strValues[i] = str
+						} else {
+							return nil, fmt.Errorf("expected string value for %s", key)
+						}
+					}
+					return strValues, nil
+				}
+			}
+		}
+		return nil, fmt.Errorf("property %s not found", key)
+	}
+	return nil, fmt.Errorf("properties is not a valid format")
+}
 
 const RegisterJsonName = "eba_register.json"
 
 var now = time.Now()
-
-// func serviceFromString(str string) (Service, error) {
-// 	switch str {
-// 	case "PS_080":
-// 		return AIS, nil
-// 	case "PS_070":
-// 		return PIS, nil
-// 	}
-// 	return "", fmt.Errorf("unknown service: %s", str)
-// }
 
 func getRegistry() error {
 	reader, err := downloadRegistry()
@@ -133,28 +337,29 @@ func parseRegistry() (<-chan models.TPP, error) {
 
 	res := make(chan models.TPP)
 	go func() {
-		var registry [][]models.TPP
+		var registry [][]RawTPP
 		err = json.Unmarshal(file, &registry)
 		if err != nil {
 			log.Fatal(err)
 		}
 		for _, tpps := range registry {
-			for _, tpp := range tpps {
-				if tpp.Id == "" {
+			for _, rawTpp := range tpps {
+				if rawTpp.CA_OwnerID == "" || rawTpp.Code == "" {
+					// log.Printf("Skipping TPP with missing CA_OwnerID or Code: %+v\n", rawTpp)
 					continue
 				}
-				if tpp.Type == "" {
+				if rawTpp.Type == "" {
+					// log.Printf("Skipping TPP with missing Type: %+v\n", rawTpp)
 					continue
 				}
-				if tpp.NameLatin == "" {
+				if rawTpp.AuthorizedAt.IsZero() {
+					// log.Printf("Skipping TPP with missing AuthorizedAt: %+v\n", rawTpp)
 					continue
 				}
-				if len(tpp.Services) == 0 {
+				if !slices.Contains([]string{"PSD_AISP", "PSD_PI", "PSD_EMI"}, rawTpp.Type) {
 					continue
 				}
-				tpp.CreatedAt = now
-				tpp.UpdatedAt = now
-				res <- tpp
+				res <- rawTpp.toTPP()
 			}
 		}
 		close(res)
@@ -225,7 +430,7 @@ func main() {
 	// 4. Parse the file
 	// 5. Save the parsed data to the DB
 
-	getRegistry()
+	// getRegistry()
 	tppChan, err := parseRegistry()
 	if err != nil {
 		log.Fatal(err)
