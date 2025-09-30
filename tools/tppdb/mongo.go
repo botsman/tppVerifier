bbae546e80b9563778e735ed32a1967cb850e497
Pavel Botsman <11509664+botsman@users.noreply.github.com>
2025-09-30 15:14:05 +0300

    optimize memory consumption (#51)

--- tools/tppdb/mongo.go
+++ tools/tppdb/mongo.go
@@ -2,6 +2,8 @@ package tppdb
 
 import (
 	"context"
+	"errors"
+	"strings"
 
 	"github.com/botsman/tppVerifier/app/models"
 	"go.mongodb.org/mongo-driver/bson"
@@ -25,14 +27,27 @@ func setupMongoDb(ctx context.Context, connStr string) (*MongoDb, error) {
 	if err != nil {
 		return nil, err
 	}
-	db := client.Database(opts.Auth.AuthSource)
-	if db == nil {
-		db = client.Database("tppVerifier")
+	dbName, err := extractDatabaseName(connStr)
+	if err != nil {
+		return nil, err
 	}
+	db := client.Database(dbName)
 	return &MongoDb{
 		Client:   client,
 		Database: db,
 	}, nil
+}
+
+func extractDatabaseName(connStr string) (string, error) {
+	opts := options.Client().ApplyURI(connStr)
+	if opts.Auth != nil && opts.Auth.AuthSource != "" {
+		return opts.Auth.AuthSource, nil
+	}
+	connStrParts := strings.Split(connStr, "/")
+	if len(connStrParts) < 2 {
+		return "", errors.New("database name not found in connection string")
+	}
+	return connStrParts[len(connStrParts)-1], nil
 }
 
 func (db *MongoDb) Disconnect(ctx context.Context) error {
